127dd53e096c84c114dbc8a3d85bb5eef4f3c623
JavierCervilla <javier@cervilla.es>
2020-08-01 22:43:15 +0200

    add sass and makeup(@ftdgomez)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './style.css';
+import './makeup/makeup.scss'
 
 const contacts = ["Mario Pernia", "Javier Quijada", "Javier Cervilla"];
 
