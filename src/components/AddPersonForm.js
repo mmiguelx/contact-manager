0335dc6ab8917dec7927f750d9c649e7ea6d79c0
mmiguelx <mmiguelx@gmail.com>
2020-08-02 23:51:08 +0200

    formatting a bit

--- src/components/AddPersonForm.js
+++ src/components/AddPersonForm.js
@@ -4,9 +4,7 @@ import '../makeup/makeup.scss'
 export function AddPersonForm(props) {
 	const[person, setPerson] = useState("");
 
-	function handleChange(e) {
-	  setPerson(e.target.value);
-	}
+	const handleChange = e => setPerson(e.target.value);
 
 	function handleSubmit(e) {
 	  if (person !== '') {
@@ -18,15 +16,20 @@ export function AddPersonForm(props) {
 
 	return (
 		<div className="flex w-full m-1 border rounded-xl">
-		  <form className="w-full card-body rounded-xl shadow flex justify-between"
+		  <form
+		  className="w-full card-body rounded-xl shadow flex justify-between"
 		  onSubmit={handleSubmit}>
-			<input type="text"
-			placeholder="Add new contact"
-			onChange={handleChange}
-			className="w-full input hover-up border rounded-xl text-l align-center w-auto m p-1"
-			value={person} />
-			<button className="btn hover-up rounded-xl shadow justify-end"
-			type="submit">add</button>
+			<input
+				type="text"
+				placeholder="Add new contact"
+				onChange={handleChange}
+				className="w-full input hover-up border rounded-xl text-l align-center w-auto m p-1"
+				value={person}
+			/>
+			<button
+				className="btn hover-up rounded-xl shadow justify-end"
+				type="submit">Add
+			</button>
 		  </form>
 		</div>
 	);
--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -4,8 +4,10 @@ import '../makeup/makeup.scss'
 export function PeopleList(props) {
 	const arr = props.data;
 	const listItems = arr.map((val, index) =>
-		<li className="hover-up rounded-xl shadow  bg-white p m text-md" key={index}>
-		  {val}
+		<li
+			className="hover-up rounded-xl shadow  bg-white p m text-md"
+			key={index}>
+			{val}
 		</li>
 	);
 	return	<div className="card w-full rounded-xl shadow p m-y">
