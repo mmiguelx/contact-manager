80c779c90871631047ec89adbb361df7a029b8d3
mmiguelx <mmiguelx@gmail.com>
2020-08-03 00:55:17 +0200

    localstorage done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './makeup/makeup.scss';
 import {AddPersonForm} from './components/AddPersonForm';
 import {PeopleList} from './components/PeopleList';
 
 export function App() {
 	const [contacts,setContacts] = useState([]);
+
+	useEffect(() => {
+		let data = localStorage.getItem('contacts');
+		if (data != null) {
+			setContacts(JSON.parse(data));
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('contacts', JSON.stringify(contacts));
+	}, [contacts]);
 
 	function addPerson(name) {
 	  setContacts([...contacts, name]);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
-import {Storage} from './components/StorageTest';
 
 ReactDOM.render(
   <div className="b-none shadow rounded-lg card w-auto  p-3 m-4">
     <App />
-    <Storage />
   </div>,
   document.getElementById('root')
 );
