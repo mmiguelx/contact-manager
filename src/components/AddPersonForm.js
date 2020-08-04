08703e435a7f09fa8ad66ba4e1ddac8a3dccc8fb
mmiguelx <mmiguelx@gmail.com>
2020-08-04 23:13:13 +0200

    some error messages added, need style

--- src/components/AddPersonForm.js
+++ src/components/AddPersonForm.js
@@ -3,15 +3,15 @@ import '../makeup/makeup.scss';
 import {useForm} from "react-hook-form";
 
 export function AddPersonForm(props) {
-	const { register, handleSubmit } = useForm();
+	const { register, handleSubmit, errors } = useForm();
 
 	/*
 	**onSubmit gets the data from ref in the form, if there's no blank box on
-	**submit parent addPerson function is called with an object data with the
-	**same structure.
+	**submit and the telephone number is unique the parent addPerson function is
+	**called with an object data with the same structure.
 	*/
-	const OnSubmit = (data, e) => {
-		if (data.name !== "" && data.tel !== "" && props.data.findIndex(obj => obj.tel === data.tel) === -1) {
+	const onSubmit = (data, e) => {
+		if (props.data.findIndex(obj => obj.tel === data.tel) === -1) {
 			props.addPerson(data);
 		}
 		e.target.reset();
@@ -21,13 +21,13 @@ export function AddPersonForm(props) {
 		<div className="flex w-full m-1 border rounded-xl">
 			<form
 				className="w-full card-body rounded-xl shadow flex justify-between"
-				onSubmit={handleSubmit(OnSubmit)}>
+				onSubmit={handleSubmit(onSubmit)}>
 				<input
 					type="text"
 					placeholder="Add new contact"
 					className="w-full input hover-up border rounded-xl text-l align-center w-auto m p-1"
 					name="name"
-					ref={register}
+					ref={register({required: true})}
 					defaultValue=""
 				/>
 				<input
@@ -35,7 +35,7 @@ export function AddPersonForm(props) {
 					placeholder="Phone Number"
 					className="w-full input hover-up border rounded-xl text-l align-center w-auto m p-1"
 					name="tel"
-					ref={register}
+					ref={register({required: true})}
 					defaultValue=""
 				/>
 				<button
@@ -43,6 +43,8 @@ export function AddPersonForm(props) {
 					type="submit">Add
 				</button>
 			</form>
+			{errors.name && "Name required"}
+			{errors.tel && "Phone required"}
 		</div>
 	);
 }
