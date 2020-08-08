7b22ea20eb33de907c55ad19deb6fcc13d3edf21
mmiguelx <mmiguelx@gmail.com>
2020-08-08 23:07:48 +0200

    format mini fix

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -78,8 +78,7 @@ export function PeopleList(props) {
 	return (
 		<div data-uk-slider>
 			<div className="uk-slider-container">
-				<ul
-					className="uk-slider-items uk-child-width-1-2@s uk-child-width-1-3@m ">
+				<ul className="uk-slider-items uk-child-width-1-2@s uk-child-width-1-3@m ">
 					{listItems}
 				</ul>
 			</div>
